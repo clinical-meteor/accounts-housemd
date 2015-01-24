f197d6334fc2e446d63e3e15a9c67c6fa64cfeeb
Abigail Watson <awatson1978@gmail.com>
2015-01-23 19:44:43 -0500

    add_files to addFiles

--- package.js
+++ package.js
@@ -5,16 +5,12 @@ Package.describe({
 Package.on_use(function (api) {
   api.add_files('bootstrap.users.js', 'server');
 
-  api.add_files('housemd/allison.camron.jpg', "client", {isAsset: true});
-  api.add_files('housemd/eric.foreman.jpg', "client", {isAsset: true});
-  api.add_files('housemd/gregory.house.jpg', "client", {isAsset: true});
-  api.add_files('housemd/james.wilson.jpg', "client", {isAsset: true});
-  api.add_files('housemd/lawrence.kutner.jpg', "client", {isAsset: true});
-  api.add_files('housemd/lisa.cuddy.jpg', "client", {isAsset: true});
-  api.add_files('housemd/robert.chase.jpg', "client", {isAsset: true});
-  api.add_files('housemd/thirteen.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/allison.camron.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/eric.foreman.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/gregory.house.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/james.wilson.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/lawrence.kutner.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/lisa.cuddy.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/robert.chase.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/thirteen.jpg', "client", {isAsset: true});
 });
-
-
-
-
