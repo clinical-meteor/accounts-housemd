169a41f951f8bf2a9ee02c8a5c167ec1e8cdb41b
Abigail Watson <awatson1978@gmail.com>
2016-09-22 15:14:30 -0500

    updated clinical:hl7-resource-practitioner

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  summary: "Adds images from House MD to the Meteor.users collection.",
-  version: "2.0.1",
-  name: "clinical:accounts-housemd",
-  git: "http://github.com/awatson1978/accounts-housemd.git",
+  summary: 'Adds images from House MD to the Meteor.users collection.',
+  version: '2.0.2',
+  name: 'clinical:accounts-housemd',
+  git: 'http://github.com/awatson1978/accounts-housemd.git',
   isDebug: true
 });
 
@@ -13,17 +13,17 @@ Package.on_use(function (api) {
   api.use('accounts-base@1.1.3');
   api.use('accounts-password@1.0.5');
 
-  api.use('clinical:hl7-resource-practitioner@1.3.0');
+  api.use('clinical:hl7-resource-practitioner@1.4.0');
   api.imply('clinical:hl7-resource-practitioner');
 
   api.addFiles('server/initialize.js', 'server');
 
-  api.addFiles('housemd/allison.camron.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/eric.foreman.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/gregory.house.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/lawrence.kutner.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/lisa.cuddy.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/robert.chase.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/james.wilson.jpg', "client", {isAsset: true});
-  api.addFiles('housemd/thirteen.jpg', "client", {isAsset: true});
+  api.addFiles('housemd/allison.camron.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/eric.foreman.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/gregory.house.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/lawrence.kutner.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/lisa.cuddy.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/robert.chase.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/james.wilson.jpg', 'client', {isAsset: true});
+  api.addFiles('housemd/thirteen.jpg', 'client', {isAsset: true});
 });
